d197607e3467298e4233d24e00348894155c6804
grandalf6 <grandalf6@gmail.com>
2023-04-12 19:46:10 +0100

    temp

--- src/utils/getAllUsers.js
+++ src/utils/getAllUsers.js
@@ -1,5 +1,6 @@
-const { USERS_JSON_PATH } = require('../config')
-const { readJson } = require('./readJson')
+const { USERS_JSON_PATH } = require("../config")
+const readJson = require("./readJson")
+const shapeUser = require("./shapeUser")
 
 const getAllUsers = async () => {
   const users = await readJson(USERS_JSON_PATH)
